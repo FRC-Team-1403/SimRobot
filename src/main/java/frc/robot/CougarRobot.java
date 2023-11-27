f4c88c0fad3f3d71488a0d22cdac24a6dae34ea8
carghai <carghai74@gmail.com>
2023-11-26 21:57:05 -0500

    rename

--- src/main/java/frc/robot/CougarRobot.java
+++ src/main/java/frc/robot/CougarRobot.java
@@ -17,7 +17,7 @@ import frc.robot.subsystems.Elevator;
 import java.util.List;
 
 public class CougarRobot extends TimedRobot {
-  private final XboxController m_controller = new XboxController(0);
+  private final XboxController m_keyboard = new XboxController(0);
 
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0
   // to 1.
@@ -68,15 +68,15 @@ public class CougarRobot extends TimedRobot {
   public void teleopPeriodic() {
     // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
-    double xSpeed = -m_speedLimiter.calculate(m_controller.getLeftY()) * Drivetrain.kMaxSpeed;
+    double xSpeed = -m_speedLimiter.calculate(m_keyboard.getLeftY()) * Drivetrain.kMaxSpeed;
 
     // Get the rate of angular rotation. We are inverting this because we want a
     // positive value when we pull to the left (remember, CCW is positive in
     // mathematics). Xbox controllers return positive values when you pull to
     // the right by default.
-    double rotaion = -m_rotLimiter.calculate(m_controller.getLeftX()) * Drivetrain.kMaxAngularSpeed;
+    double rotaion = -m_rotLimiter.calculate(m_keyboard.getLeftX()) * Drivetrain.kMaxAngularSpeed;
     m_drive.drive(xSpeed, rotaion);
-    if (m_controller.getAButton()) {
+    if (m_keyboard.getAButton()) {
       // Here, we set the constant setpoint of 0.75 meters.
       m_elevator.reachGoal(Constants.kSetpointMeters);
     } else {
