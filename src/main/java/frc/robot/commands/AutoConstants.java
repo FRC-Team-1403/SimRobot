5b5eb617e61f97892b1cd13253962fccce6817e9
devastate347 <115187508+devastate347@users.noreply.github.com>
2024-01-02 15:46:20 -0500

    moved AutoConstants.java out of commands folder

--- src/main/java/frc/robot/commands/AutoConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.commands;
-
-public class AutoConstants {
-    final static double waitTimeSeconds = 1.2;
-    final static double speed = 1;
-    final static double rotation = 0.2;
-
-}
