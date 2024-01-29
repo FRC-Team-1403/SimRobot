09be2a170b31cf50f1d99b49367df1b4b75e5e34
carghai <carghai74@gmail.com>
2024-01-29 17:35:23 -0500

    part commit

--- algo.java
+++ algo.java
@@ -1,30 +1,81 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 class algo {
-    private final Map<Double, ShooterValues> table = new TreeMap<>();
+    private final Map<Double, Map<Double, ShooterValues>> table = new TreeMap<>();
 
     public algo() {
-        table.put(2.0, new ShooterValues(15, 20, 0));
-        table.put(3.0, new ShooterValues(20, 30, 1));
-        table.put(4.0, new ShooterValues(25, 40, 2));
-        table.put(5.0, new ShooterValues(30, 50, 3));
-        table.put(6.0, new ShooterValues(35, 60, 4));
-        table.put(7.0, new ShooterValues(40, 70, 5));
-        table.put(8.0, new ShooterValues(45, 80, 6));
-        table.put(9.0, new ShooterValues(50, 90, 6));
+
 
     }
 
     public ShooterValues compute(double location) {
-        ShooterValues lowData = null;
-        ShooterValues highData = null;
-        double lowDataDistance = Double.NEGATIVE_INFINITY;
-        double highDataDistance = Double.POSITIVE_INFINITY;
+        Values<Map<Double, ShooterValues>> ypoints = new Values<Map<Double, ShooterValues>>();
+        ypoints.findCloset(table, location);
+        Values<ShooterValues> pointsHigh = new Values<ShooterValues>();
+        pointsHigh.findCloset(ypoints.high, location);
+        Values<ShooterValues> pointsLow = new Values<ShooterValues>();
+        pointsLow.findCloset(ypoints.low, location);
+        // we have points now calc time
+        ShooterValues high = pointsHigh.high.interpolateOther(pointsHigh.low, pointsHigh.highDataDistance, pointsHigh.lowDataDistance);
+        ShooterValues low = pointsLow.high.interpolateOther(pointsLow.low, pointsLow.highDataDistance, pointsLow.lowDataDistance);
+        ShooterValues 
+        return new ShooterValues(interpolate(highData.angle, highDataDistance, lowData.angle, lowDataDistance),
+                interpolate(highData.rpm, highDataDistance, lowData.rpm, lowDataDistance),
+                interpolate(highData.offset, highDataDistance, lowData.offset, lowDataDistance));
+    }
 
-        for (Map.Entry<Double, ShooterValues> entry : table.entrySet()) {
+    public static void main(String[] args) {
+        System.out.println(new algo().compute(2.2));
+    }
+}
+
+class ShooterValues {
+    public double angle;
+    public double rpm;
+    public double robotAngle;
+
+    public ShooterValues interpolateOther(ShooterValues other, double selfDistance, double otherDistance) {
+        return new ShooterValues(interpolate(this.angle, selfDistance, other.angle, otherDistance),
+        interpolate(this.rpm, selfDistance, other.rpm, otherDistance),
+        interpolate(this.robotAngle, selfDistance, other.robotAngle, otherDistance));
+     }
+
+    private double interpolate(double highData, double highDataDistance, double lowData, double lowDataDistance) {
+        return ((highData / highDataDistance) + Math.abs(lowData / lowDataDistance)) *
+                Math.abs(highDataDistance * lowDataDistance);
+    }
+    public ShooterValues(double angle, double rpm, double offset) {
+        this.angle = angle;
+        this.rpm = rpm;
+        this.robotAngle = offset;
+    }
+
+    public String toString() {
+        return "angle: " + angle + " rpm: " + rpm + " robotAngle: " + robotAngle;
+    }
+}
+
+class Values<T> {
+    public T high;
+    public T low;
+    public double highDataDistance = Double.POSITIVE_INFINITY;
+    public double  lowDataDistance = Double.NEGATIVE_INFINITY;
+    public Values(T high, T low) {
+        this.high = high;
+        this.low = low;
+    }
+
+    public Values() {
+    }
+
+    public void findCloset(Map<Double, T> data, double location) {
+        T lowData = null;
+        T highData = null;
+        for (Map.Entry<Double, T> entry : data.entrySet()) {
             double key = entry.getKey();
-            ShooterValues value = entry.getValue();
+            T value = entry.getValue();
             double check = key - location;
             // if key negative then it lower
             if (check < 0 && check > lowDataDistance) {
@@ -39,36 +90,7 @@ class algo {
                 break;
             }
         }
-
-        System.out.println("Low Data: " + lowData + ", High Data: " + highData +
-                           ", Low Data Dist: " + lowDataDistance + ", High Data Dist: " + highDataDistance);
-        return new ShooterValues(interpolate(highData.angle, highDataDistance, lowData.angle, lowDataDistance),
-                interpolate(highData.rpm, highDataDistance, lowData.rpm, lowDataDistance),
-                interpolate(highData.offset, highDataDistance, lowData.offset, lowDataDistance));
-    }
-
-    private double interpolate(double highData, double highDataDistance, double lowData, double lowDataDistance) {
-        return ((highData / highDataDistance) + Math.abs(lowData / lowDataDistance)) *
-                Math.abs(highDataDistance * lowDataDistance);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new algo().compute(2.5));
-    }
-}
-
-class ShooterValues {
-    public double angle;
-    public double rpm;
-    public double offset;
-
-    public ShooterValues(double angle, double rpm, double offset) {
-        this.angle = angle;
-        this.rpm = rpm;
-        this.offset = offset;
-    }
-
-    public String toString() {
-        return "angle: " + angle + " rpm: " + rpm + " offset: " + offset;
+        this.high = highData;
+        this.low = lowData;
     }
 }
